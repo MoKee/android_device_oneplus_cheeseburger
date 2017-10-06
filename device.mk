69cfa77833a53dafe9cdc226fc189ea4b8dd1ec0
XiNGRZ <chenxingyu92@gmail.com>
2017-10-06 20:13:16 +0800

    cheeseburger: HIDL: Build android.hardware.memtrack@1.0-impl
    Change-Id: I60bfbb19ef2b3b502ea15f01453c6a156a5d71c3

--- device.mk
+++ device.mk
@@ -185,6 +185,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-service \
+    android.hardware.memtrack@1.0-impl \
     copybit.msm8998 \
     gralloc.msm8998 \
     hwcomposer.msm8998 \
