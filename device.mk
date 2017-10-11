d61c2d68afec5c388aa25610365f8b7d97c25e44
XiNGRZ <chenxingyu92@gmail.com>
2017-10-12 00:54:15 +0800

    cheeseburger: Move non-AOSP overlays to overlay-mokee
    * So that they can be easily disabled while bringing up new major
      Android releases.
    
    Change-Id: I818b2a858b448138b26e0cda9b893a7d27f01ac8

--- device.mk
+++ device.mk
@@ -26,6 +26,7 @@ $(call inherit-product, vendor/oneplus/cheeseburger/cheeseburger-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-mokee
 
 # Permissions
 PRODUCT_COPY_FILES += \
