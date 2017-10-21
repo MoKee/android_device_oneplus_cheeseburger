f22f40617bf06477e087a0c4b4b4fb728a018016
XiNGRZ <chenxingyu92@gmail.com>
2017-10-21 13:12:39 +0800

    cheeseburger: Removed Sensors HIDL
    * Use MultiHal instead. Don't know why.
    
    Change-Id: I0fda37de20203458e411a836e2942b61428ef4ef

--- device.mk
+++ device.mk
@@ -376,7 +376,6 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service \
     sensors.cheeseburger
 
 # SOTER (Fingerprint support for WeChat Payment)
