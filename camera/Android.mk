d78f50712e2166a570e19d3640fc63aafd283dfb
XiNGRZ <chenxingyu92@gmail.com>
2017-12-24 19:36:55 +0800

    cheeseburger: Reverted CameraParameters.cpp to frameworks/av/camera
    Change-Id: I8eff5fd2139c27eff473f23c77ccf72a4b1b39bc

--- camera/Android.mk
+++ camera/Android.mk
@@ -16,13 +16,9 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_C_INCLUDES := \
-    frameworks/av/include
-
 LOCAL_SRC_FILES := \
     CameraParameters.cpp
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
 LOCAL_MODULE := libcamera_parameters_ext
 LOCAL_MODULE_TAGS := optional
 
