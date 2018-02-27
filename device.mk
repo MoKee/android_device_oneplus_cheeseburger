a17ebe2324865bf2fe29981a6ccfc6a87c6570f2
Hamster Tian <haotia@gmail.com>
2018-02-27 22:03:14 +0800

    cheeseburger: re-enable and clean up overlay
    Change-Id: I6b8f725aa5fc5cd49d17471383a11808d450159f

--- device.mk
+++ device.mk
@@ -25,7 +25,7 @@
 $(call inherit-product, vendor/oneplus/cheeseburger/cheeseburger-vendor.mk)
 
 # Overlays
-# DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-mokee
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-mokee
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
